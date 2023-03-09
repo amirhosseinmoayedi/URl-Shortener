5779332e08eb1e6c4d53325e9ea840049368c239
Amir Hossein Moayedo <amir.h.moayedi@gmail.com>
2023-03-09 21:10:13 +0330

    Feat: adding di for starting the project

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"url-shortener/internal/cache"
+	"url-shortener/internal/http"
+	url_shortner "url-shortener/internal/url-shortner"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	// init repo
+	repo := cache.NewInMemoryCacheURLRepository()
+	// init service
+	service, err := url_shortner.NewService(repo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// init handler
+	var handler *url_shortner.Handler
+	handler, err = url_shortner.NewHandler(*service)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// init router
+	var router *http.Router
+	router, err = http.NewRouter(*handler)
+	// serve
+	router.Serve()
 }
