a08d54e7201de26175c808d951ed12b91c853ecc
Amir Hossein Moayedo <amir.h.moayedi@gmail.com>
2023-03-09 18:53:58 +0330

    refactor: file service and url seperated and some minor code changes
    url struct entity moved to url file
    adding validation to NewService

---
+++ internal/url-shortner/service.go
@@ -0,0 +1,38 @@
+package url_shortner
+
+import (
+	"context"
+	"errors"
+)
+
+type Service struct {
+	URLRepo URLRepository
+}
+
+func NewService(repo URLRepository) (*Service, error) {
+	if repo == nil {
+		return nil, errors.New("URL repository cant be nil")
+	}
+	return &Service{URLRepo: repo}, nil
+}
+
+func (s Service) ShortenLink(ctx context.Context, url *URL) error {
+	if err := url.validateOriginalPath(); err != nil {
+		return err
+	}
+	if err := url.setCreateAt(); err != nil {
+		return err
+	}
+	if err := s.URLRepo.add(ctx, *url); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s Service) GetShortenLink(ctx context.Context, path string) (*URL, error) {
+	url, err := s.URLRepo.find(ctx, path)
+	if err != nil {
+		return nil, err
+	}
+	return url, nil
+}
