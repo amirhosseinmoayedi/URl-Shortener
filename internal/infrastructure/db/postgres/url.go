cf38f153c4247d8a5b8ea549aeaba531276075b3
Amir Hossein Moayedo <amir.h.moayedi@gmail.com>
2023-03-13 19:32:29 +0330

    Feat: add postgres url repository
    add implementation of URLRepository base on postgres to infrastructure layer

---
+++ internal/infrastructure/db/postgres/url.go
@@ -0,0 +1,59 @@
+package postgres
+
+import (
+	"context"
+	"errors"
+	"github.com/amirhosseinmoayedi/URl-Shortener/internal/domain/entity"
+	"github.com/amirhosseinmoayedi/URl-Shortener/internal/domain/repository"
+	"gorm.io/gorm"
+)
+
+type URLRepositoryPostgres struct {
+	db *gorm.DB
+}
+
+type URLPostgres struct {
+	gorm.Model
+	Path     string `gorm:"uniqueIndex:idx_path,not null"`
+	Original string `gorm:"unique"`
+}
+
+func NewPostgresURLRepository(db *gorm.DB) *URLRepositoryPostgres {
+	return &URLRepositoryPostgres{
+		db: db,
+	}
+}
+
+func (p *URLRepositoryPostgres) Add(ctx context.Context, url entity.URL) error {
+	postgresURl := URLPostgres{
+		Path:     url.Path,
+		Original: url.Original,
+	}
+
+	db := p.db.WithContext(ctx)
+
+	result := db.Create(&postgresURl)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrDuplicatedKey) {
+			return repository.URLAlreadyExists
+		} else {
+			return result.Error
+		}
+	}
+	return nil
+}
+
+func (p *URLRepositoryPostgres) Find(ctx context.Context, path string) (entity.URL, error) {
+	var url URLPostgres
+	db := p.db.WithContext(ctx)
+	result := db.Where("path = ?", path).First(&url)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return entity.URL{}, repository.URLNotFound
+		} else {
+			return entity.URL{}, result.Error
+		}
+	}
+
+	return entity.URL{Path: url.Path, Original: url.Original}, nil
+}
--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
-	service2 "github.com/amirhosseinmoayedi/URl-Shortener/internal/domain/service"
-	"github.com/amirhosseinmoayedi/URl-Shortener/internal/infrastructure/db/memory"
+	"github.com/amirhosseinmoayedi/URl-Shortener/internal/domain/service"
+	"github.com/amirhosseinmoayedi/URl-Shortener/internal/infrastructure/db/postgres"
 	"github.com/amirhosseinmoayedi/URl-Shortener/internal/interface/http"
 	urlshortner "github.com/amirhosseinmoayedi/URl-Shortener/internal/interface/http/v1"
+	"github.com/amirhosseinmoayedi/URl-Shortener/internal/log"
+	gormPostgres "gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
-	repo := memory.NewInMemoryCacheURLRepository()
-	service := service2.NewService(repo)
-	handler := urlshortner.NewHandler(service)
+	//repo := memory.NewURLRepositoryInMemory()
+	dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Tehran"
+	db, err := gorm.Open(gormPostgres.New(gormPostgres.Config{
+		DSN:                  dsn,
+		PreferSimpleProtocol: true, // disables implicit prepared statement usage
+	}), &gorm.Config{})
+	if err != nil {
+		log.Logger.WithFields(map[string]interface{}{"dsn": dsn, "err": err}).Fatal("can't open the postgres connection")
+	}
+	err = db.AutoMigrate(&postgres.URLPostgres{})
+	if err != nil {
+		log.Logger.WithField("err", err).Fatal("can't make database migrate ")
+	}
+
+	repo := postgres.NewPostgresURLRepository(db)
+	srv := service.NewService(repo)
+	handler := urlshortner.NewHandler(srv)
 
 	router := http.NewServer(handler)
 	router.Serve()
