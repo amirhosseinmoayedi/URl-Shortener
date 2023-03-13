2daa7bdf34692b94053b897179039bedc36aed75
Amir Hossein Moayedo <amir.h.moayedi@gmail.com>
2023-03-13 14:59:17 +0330

    Feat: logging functions added to service layer
    logs level changed, and info logs for service layer added

--- internal/url-shortner/service.go
+++ internal/url-shortner/service.go
@@ -14,30 +14,34 @@ func NewService(repo URLRepository) *Service {
 }
 
 func (s Service) ShortenLink(ctx context.Context, url *URL) error {
+	log.Logger.WithFields(map[string]interface{}{"ctx": ctx, "url": url}).Info("request for shorten the url")
 	if err := url.validateOriginalPath(); err != nil {
-		log.Logger.WithFields(map[string]interface{}{"ctx": ctx, "url": url}).Info(err)
+		log.Logger.WithFields(map[string]interface{}{"ctx": ctx, "url": url}).Error(err)
 		return err
 	}
 	if err := url.setCreateAt(); err != nil {
-		log.Logger.WithFields(map[string]interface{}{"ctx": ctx, "url": url}).Info(err)
+		log.Logger.WithFields(map[string]interface{}{"ctx": ctx, "url": url}).Error(err)
 		return err
 	}
 	if err := url.setPath(); err != nil {
-		log.Logger.WithFields(map[string]interface{}{"ctx": ctx, "url": url}).Warn(err)
+		log.Logger.WithFields(map[string]interface{}{"ctx": ctx, "url": url}).Error(err)
 		return err
 	}
 	if err := s.URLRepo.Add(ctx, *url); err != nil {
-		log.Logger.WithFields(map[string]interface{}{"ctx": ctx, "url": url}).Warn(err)
+		log.Logger.WithFields(map[string]interface{}{"ctx": ctx, "url": url}).Error(err)
 		return err
 	}
+	log.Logger.WithFields(map[string]interface{}{"ctx": ctx, "url": url}).Info("url shorted")
 	return nil
 }
 
 func (s Service) GetShortenLink(ctx context.Context, path string) (*URL, error) {
+	log.Logger.WithFields(map[string]interface{}{"ctx": ctx, "path": path}).Info("request for get the original url")
 	url, err := s.URLRepo.Find(ctx, path)
 	if err != nil {
 		log.Logger.WithFields(map[string]interface{}{"ctx": ctx, "path": path}).Info(err)
 		return nil, err
 	}
+	log.Logger.WithFields(map[string]interface{}{"ctx": ctx, "path": path, "url": url}).Info("url short version found")
 	return &url, nil
 }
